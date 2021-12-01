54f84d0b3ecd0af941629a98f9976a56a47228cb
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-12-01 20:39:47 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,21 @@
-#include "lib.h.h"
-bool primi (int a, int d){
-   int resto=0;
-if (a>1){
-    if(d<a){
-        resto=a%d;
-        if (resto != 0) {
-            return primi(a, d + 1);
-        } else {
-            return false;
-        }
-        }else {
-        return true;
-    }
-        }else if (a == 2) {
-            return true;
-        } else {
-            return false;
-        }
+#include <iostream>
+#include "lib.h"
+bool primi(int a, int d){
+   
+   if (a<2){
+      return false;
+   }
+   if (a==2){
+         return true;
+      }
 
+       int resto=a%d;
+        if (resto == 0) {
+            return false;
+        }
+      if (b>2){
+         bool risultato= primi (a,d-1);
+         return risultato
+         }
+   return true ;
 }
