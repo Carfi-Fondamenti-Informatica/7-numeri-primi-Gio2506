2c272474449619564b59bc9ef0f35da998ee2b83
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-12-01 20:22:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a=0;
+    int b=2;
+    bool risultato=primi (a,b);
+    cin>>a;
+    if(risultato){
+        cout<<"numero primo"<<endl;
+    }else{
+        cout<<"numero non primo"<<endl;
+    }
+    return 0;
 }
